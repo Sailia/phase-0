cd405adeaf45fbbc9f1f1800bfc8bfd8741fb9ae
Sailia <syema.ailia@gmail.com>
2015-10-01 00:30:47 -0500

    upload of 4.3 Variables and Methods

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,27 @@
+# Math Methods
+
+# I worked on this challenge [by myself].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ puts num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+ puts num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+ puts num_1 * num_2
+end
+
+def divide(num_1, num_2)
+ puts num_1 / num_2.to_f
+end
+
+add(5, 6)
+subtract(6, 3)
+multiply(40, 2)
+divide(1000, 5)
