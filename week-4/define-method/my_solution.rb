2f167e001bc80b1d61a9f1cc3dac23b60b53e4de
Sailia <syema.ailia@gmail.com>
2015-10-01 00:46:49 -0500

    variable methods 4.3

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,12 @@
+
+# Define an Empty Method
+
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def do_something(m1, m2, m3)
+
+end
---
+++ week-4/triangle-side/my_solution.rb
@@ -0,0 +1,12 @@
+# I worked on this challenge [by myself, with: Ronny].
+
+# Your Solution Below
+
+def valid_triangle?(a, b, c)
+  puts (a + b > c) && (b + c > a) && (a + c > b)
+end
+
+valid_triangle?(3, 6, 8)
+
+
+
