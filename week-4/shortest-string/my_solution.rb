dc2ad1e0669fa341417c892f4965e37e62cf429a
Sailia <syema.ailia@gmail.com>
2015-10-03 23:23:39 -0500

    initial upload of shortest string

---
+++ week-4/shortest-string/my_solution.rb
@@ -0,0 +1,16 @@
+# Shortest String
+
+# I worked on this challenge [by myself, with: ].
+
+# shortest_string is a method that takes an array of strings as its input
+# and returns the shortest string
+#
+# +list_of_words+ is an array of strings
+# shortest_string(array) should return the shortest string in the +list_of_words+
+#
+# If +list_of_words+ is empty the method should return nil
+
+#Your Solution Below
+def shortest_string(list_of_words)
+  list_of_words.sort_by(&:length)[0]
+end
