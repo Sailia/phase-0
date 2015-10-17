af12d330e6f910fe891c7c1a28a6a1d854f2343d
Sailia <syema.ailia@gmail.com>
2015-10-17 14:51:08 -0500

    second upload reflect add

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -94,3 +94,22 @@ end
 
 
 # Reflection
+=begin
+
+How do instance variables and methods represent the characteristics and behaviors (actions) of a real-world object?
+
+The best way I could think of that'd can best explain instance variables and methods is this: There's a class called Cat. All cats miow, so theres an instance variable called miow. But the methods are what give each miow a personality trait.
+
+When should you use instance variables? What do they do for you?
+
+Instance variables help you use the same variable in multiple methods. Each method contains instructions that you can use on the same instance variable throughout the class. They are like steps for the instance variable but in a more organised setting and to promote readability and prevent conflicts with object types.
+
+Explain how to use flow control. Did you have any trouble using it in this challenge? If so, what did you struggle with?
+
+I had no issues with flow control other than the part where I had to add the recent guess information. I had no idea how to link the two together. I found out that you can refer to other methods in the class just by calling the method.
+
+Why do you think this code requires you to return symbols? What are the benefits of using symbols?
+
+Symbols take up less memory, have one object ID so they cannot be changed and are easy to refer to. They can also point to another object.
+
+=end
