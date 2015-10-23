810a25b7186d462d8709dc34b8d2b27399273a73
Sailia <syema.ailia@gmail.com>
2015-10-22 22:42:33 -0500

    4.3 repeat

---
+++ week-4/address2/my_solution2.rb
@@ -0,0 +1,22 @@
+# Format an Address
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+=begin
+def make_address(street, city, state, zip)
+ puts "You live at #{street}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
+end
+
+make_address("633 Folsom St.", "San Francisco", "CA", "94107")
+
+=end
+
+
+def make_address(street, city, state, zip)
+   "You live at #{street}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
+end
+
+make_address("633 Folsom St.", "San Francisco", "CA", "94107")
---
+++ week-4/math2/my_solution2.rb
@@ -0,0 +1,23 @@
+# Math Methods
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+ num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+ num_1 * num_2
+end
+
+def divide(num_1, num_2)
+ num_1 / num_2
+end
+
