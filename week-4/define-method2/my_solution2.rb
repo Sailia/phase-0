8c24176fd4378e5c51122ebeef12fb51ab7416e3
Sailia <syema.ailia@gmail.com>
2015-10-22 19:49:31 -0500

    4.3 repeat

---
+++ week-4/define-method2/my_solution2.rb
@@ -0,0 +1,10 @@
+# Define an Empty Method
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def do_something(arg1, arg2, arg3)
+
+end
---
+++ week-4/variables-methods2.rb
@@ -0,0 +1,14 @@
+puts "What's your first name?"
+first_name = gets.chomp
+puts "What's your middle name?"
+middle_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+puts
+puts "Hello #{first_name} #{middle_name} #{last_name}"<< "!"
+
+
+puts "What's your favourite number?"
+num = gets.chomp.to_i
+puts "I like #{num += 1} because its bigger. Bigger means better."
+
