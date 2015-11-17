9344bca21b466603f34efd84f073efeb7633a8fe
Sailia <syema.ailia@gmail.com>
2015-11-17 17:09:54 -0600

    eloquent stuff

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -8,7 +8,10 @@
 var apple = 1;
 console.log(apple);
 
-
+prompt("What's your favourite food?")
+"Chipotle"
+confirm("That's my favourite food too!")
+true
 // Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
 
 
@@ -17,7 +20,17 @@ console.log(apple);
 
 // Complete the `minimum` exercise.
 
+function min(num1, num2) {
+ return(Math.min(num1, num2));
+}
+​
+console.log(min(0, 10));
 
+console.log(min(0, -10));
+
+-10
 
 // Data Structures: Objects and Arrays
 // Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+var me = {name: "Syema", age: 26, favouriteFoods: "Chipotle", quirk: "I will give zero fox today."}
